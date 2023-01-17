786239b299f17d9aac78cbf89ad405760cc777f7
Thyago Lobato <thyagollobato@gmail.com>
2023-01-17 19:39:43 -0300

    feat: Implementntação da lógica dos clicks no menu de frases

--- app/src/main/java/com/lobato/motivation/infra/MotivationConstants.kt
+++ app/src/main/java/com/lobato/motivation/infra/MotivationConstants.kt
@@ -5,4 +5,10 @@ class MotivationConstants private constructor(){
     object KEY {
         const val USER_NAME = "USER_NAME"
     }
+
+    object FILTER {
+        const val ALL = 1
+        const val HAPPY = 2
+        const val SUNNY = 3
+    }
 }
--- app/src/main/java/com/lobato/motivation/ui/MainActivity.kt
+++ app/src/main/java/com/lobato/motivation/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package com.lobato.motivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import com.lobato.motivation.infra.MotivationConstants
 import com.lobato.motivation.R
 import com.lobato.motivation.infra.SecurityPreferences
@@ -11,6 +12,7 @@ import com.lobato.motivation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var categoryId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,14 +23,42 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         supportActionBar?.hide()
 
         handleUserName()
+        handleFilter(R.id.image_all)
 
         // Eventos
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
             var s = ""
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
+            handleFilter(view.id)
+        }
+    }
+
+    private fun handleFilter(id: Int) {
+
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.dark_purple))
+
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.ALL
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
         }
     }
 
